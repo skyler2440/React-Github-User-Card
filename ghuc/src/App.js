934d05194983de2024347cda6f499d5cc2877bfc
skyler <skylerwebdev@gmail.com>
2019-08-06 16:48:22 -0400

    mvp w/o style

--- ghuc/src/App.js
+++ ghuc/src/App.js
@@ -6,20 +6,21 @@ class App extends React.Component {
     super();
     // we would not have access to `this` w/out you super!
     this.state = {
-      userCard:[]
+      userCard:[],
+      followerCard:[],
     };
   }
   // state = {}
-
+  
   componentDidMount() {
     this.fetchUser();
   }
-
+ 
   
   fetchUser = () => {
     fetch(`https://api.github.com/users/skyler2440`)
-      .then(response => {
-        // first promise resolution is used to format the data.
+    .then(response => {
+      // first promise resolution is used to format the data.
         return response.json();
       })
       .then(users => this.setState({ userCard: users }))
@@ -27,14 +28,15 @@ class App extends React.Component {
         console.log(err);
         // HANDLE THIS ERROR
       });
-  };
+    };
+    
   render() {
 console.log(this.state.userCard
   )
     return (
       <div>
-        <h1>GitHub Users</h1>
-       <UserCard user={this.state.userCard} />
+        <h1>GitHub Stats</h1>
+        <UserCard user={this.state.userCard} />
       </div>
     );
   }
--- ghuc/src/components/UserCard.js
+++ ghuc/src/components/UserCard.js
@@ -1,14 +1,51 @@
 import React from 'react';
 
-const UserCard = props => {
-    
-    console.log(props)
+class UserCard extends React.Component {
+  constructor(props){
+      super(props)
+      this.state={
+        followersCard: []
+      }
+  }
+  componentDidMount() {
+      this.fectchFollowers()
+  }
+  fectchFollowers = () =>{
+      fetch(`https://api.github.com/users/skyler2440/followers`)
+      .then(res => {
+          return res.json();
+      })
+      .then(followers => this.setState({ followersCard: followers}))
+      .catch(err => {
+          console.log(err)
+      })
+  }
+    render() {
+    console.log(this.state.followersCard)
     return(
         <>
-        <h1>{props.user.login}</h1>
-        <img src={props.user.avatar_url} alt='avatar'/>
+        <div className='card'>
+        <h2>Username: {this.props.user.login || 'N/A'}</h2>
+        <img src={this.props.user.avatar_url } alt='avatar'/>
+        <h3>Location: {this.props.user.location || 'N/A'}</h3>
+        <h3><a href={this.props.user.html_url || 'N/A'}>Checkout Profile Here</a></h3>
+        <h3>Following {this.props.user.following || 'N/A'} users</h3>
+        <h3>Has {this.props.user.followers || 'N/A'} followers</h3>
+        <h3>Bio: {this.props.user.bio || 'N/A'}</h3>
+        <h1>User's GitHub Followers</h1>
+        </div>
+        {this.state.followersCard.map(mres => {
+            return (
+                <>
+            <h2 key={mres.login}>Username: {mres.login}</h2>
+            <img key={mres.avatar_url} src={mres.avatar_url} alt='avatar'/>
+        <h3><a href={mres.html_url || 'N/A'}>Checkout Profile Here</a></h3>
+
+            </>
+            )
+        })}
         </>
     )
-}
+}}
 
 export default UserCard;
